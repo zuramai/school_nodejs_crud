709897bb5559d8641a0c356db4522be3f9019558
Abdurrahman <45091988+abramsmkn10@users.noreply.github.com>
2019-10-01 09:28:47 +0700

    Update TugasController.js

--- controllers/TugasController.js
+++ controllers/TugasController.js
@@ -1,14 +1,14 @@
 let db = require('./../config/connection');
 
-exports.index = (req, res) => {
-	db.query("SELECT * FROM tugas", (err, result) => {
+exports.index = (req, res) => {, tugas.deskripsi,
+	db.query("SELECT tugas.id, tugas.judul, tugas.deskripsi, tugas.kelas_id, tugas.mapel_id, kelas.nama as nama_kelas, mapel.nama as nama_mapel, kelas.lokasi FROM tugas INNER JOIN kelas ON tugas.kelas_id = kelas.id, mapel.lokasi FROM tugas INNER JOIN mapel ON tugas.mapel_id = mapel.id", (err, result) => {
 		if (err) res.status(404).send({'Error : ': err})
 			else res.status(200).send({'Result: ': result})
 	})
 }
 
 exports.detail = (req, res) => {
-	db.query("SELECT * FROM tugas WHERE id=?", [req.params.id], (err, result) => {
+	db.query("SELECT tugas.id, tugas.judul, tugas.deskripsi, tugas.kelas_id, tugas.mapel_id, kelas.nama as nama_kelas, mapel.nama as nama_mapel, kelas.lokasi FROM users INNER JOIN kelas ON tugas.kelas_id = kelas.id, mapel.lokasi FROM tugas INNER JOIN mapel ON tugas.mapel_id = mapel.id WHERE tugas.id=?", [req.params.id], (err, result) => {
 		if (err) res.status(404).send({'Error : ': err})
 			else res.status(200).send({'Result: ': result})	
 	})
@@ -17,10 +17,12 @@ exports.detail = (req, res) => {
 exports.store = (req, res) => {
 	let tugas = {
 		judul: req.body.judul,
-		deskripsi: req.body.deskripsi
+		deskripsi: req.body.deskripsi,
+		kelas_id: req.body.kelas_id,
+		mapel_id: req.body.mapel_id
 	}
 
-	db.query("INSERT INTO tugas (judul,deskripsi) VALUES (?,?)", [tugas.judul, tugas.deskripsi], (err, result) => {
+	db.query("INSERT INTO tugas (judul,deskripsi,kelas_id,mapel_id) VALUES (?,?,?,?)", [tugas.judul, tugas.deskripsi, tugas.kelas_id, tugas.mapel_id], (err, result) => {
 		if (err) res.status(500).send({'Error : ': err})
 			else res.status(201).send({'Result : ': result})
 	})
@@ -30,10 +32,12 @@ exports.update = (req, res) => {
 	let tugas = {
 		id: req.params.id,
 		judul: req.body.judul,
-		deskripsi: req.body.deskripsi
+		deskripsi: req.body.deskripsi,
+		kelas_id: req.body.kelas_id,
+		mapel_id: req.body.mapel_id
 	}
 
-	db.query("UPDATE tugas SET judul=?, deskripsi=? WHERE id=?", [tugas.judul,tugas.deskripsi, tugas.id], (err, result) => {
+	db.query("UPDATE tugas SET judul=?, deskripsi=?, kelas_id=?,mapel_id=? WHERE id=?", [tugas.judul,tugas.deskripsi,tugas.kelas_id,tugas.mapel_id, tugas.id], (err, result) => {
 		if (err) res.status(404).send({'Error : ': err})
 			else res.status(200).send({'Result: ': result})
 	})
